cde338078e8de71baadf27a012a67fcf68c700ea
jrbella <jbella@icstars.org>
2021-07-07 21:14:54 -0700

    adding test cases

--- Projects/DinnerMenu/app.js
+++ Projects/DinnerMenu/app.js
@@ -31,8 +31,8 @@ const menu = {
         deserts: this._courses.deserts
       }
     },
-    addDishToCourse(courseName, dishName, dishPrice){
-      this._courses[courseName].push({dishName, dishPrice})
+    addDishToCourse(courseName, name, price){
+      this._courses[courseName].push({name, price})
     },
     getRandomDishFromCourse(courseName){
       const dishes = this._courses[courseName]
@@ -45,15 +45,41 @@ const menu = {
       const desert = this.getRandomDishFromCourse('deserts')
       const totalPrice = appetizer.price + main.price + desert.price
   
+      // log block
+      console.log(appetizer)
+      console.log(main)
+      console.log(desert)
+  
       return `Your meal is ${appetizer.name}, ${main.name}, ${desert.name}, The price is $${totalPrice}`
   
     }
   }
   
+    
+  // mocking test data
+  // adding appetizers
+  
   menu.addDishToCourse('appetizers', 'Caesar Salad', 4.25);
+  menu.addDishToCourse('appetizers', 'French Fries', 6.25);
+  menu.addDishToCourse('appetizers', 'Chicken Soup', 7.25);
+  
+  
+  // adding main courses
+  
+  menu.addDishToCourse('mains', 'Baked Salmon', 14.25);
+  menu.addDishToCourse('mains', 'Grilled Chicken', 12.25);
+  menu.addDishToCourse('mains', 'Club Sandwich', 11.25);
+  
+  // adding deserts
+  
+  menu.addDishToCourse('deserts', 'Fudge Brownie', 4.25);
+  menu.addDishToCourse('deserts', 'Apple Pie', 3.25);
+  menu.addDishToCourse('deserts', 'Ice Cream', 7.25);
+  
   
   console.log(menu.courses)
   
   // test block
-  
+  let meal = menu.generateRandomMeal()
+  console.log(meal)
   
